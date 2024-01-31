9c5a7bb8354e2a51655b6ecc65bc033d933c9eb0
Vinzent Rittel <vinzentrittel@uni-koblenz.de>
2024-02-01 00:02:00 +0100

    add transformation matrix to and from bounding box

--- src/bounding_box.py
+++ src/bounding_box.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import Tuple
 
-from numpy import array, argmax, argmin, cross, inf, ndarray, pi
+from numpy import array, argmax, argmin, cross, identity, ndarray, pi
 from numpy.linalg import norm
 from scipy.spatial.transform import Rotation
 from vtk import vtkPolyData
@@ -21,13 +21,15 @@ class BoundingBox:
         self.secondary_normal = outer_points_second_order[1] - outer_points_second_order[0]
         self.secondary_normal /= norm(self.secondary_normal)
 
-        self.ternary_normal = cross(self.primary_normal, self.secondary_normal)
+        self.ternary_normal = cross(self.secondary_normal, self.primary_normal)
         self.ternary_normal /= norm(self.ternary_normal)
 
         self.secondary_normal = cross(self.ternary_normal, self.primary_normal)
         self.secondary_normal /= norm(self.secondary_normal)
 
         self.length = self.calc_lengths()
+        self.inverse_matrix = self.calc_normalization_matrix()
+        self.transform_matrix = self.calc_inverse_normalization_matrix()
 
     def calc_outer_points_first_order(self) -> ndarray:
         min_ = [ argmin(self.geometry[:, dimension]) for dimension in range(3) ]
@@ -72,9 +74,51 @@ class BoundingBox:
         min_ = projections.min(axis=1)
         max_ = projections.max(axis=1)
         return max_ - min_ 
+
+    def calc_normalization_matrix(self) -> ndarray:
+        translation = self.center_of_mass
+        scale = 2.0 / self.length
+
+        # axes
+        primary = scale[0] * self.primary_normal
+        secondary = scale[1] * self.secondary_normal
+        ternary = scale[2] * self.ternary_normal
         
-    def __len__(self) -> Tuple[float, float, float]:
-        return self.length
+        return array([
+            [
+                primary[0],
+                secondary[0],
+                ternary[0],
+                translation[0],
+            ], [
+                primary[1],
+                secondary[1],
+                ternary[1],
+                translation[1],
+            ], [
+                primary[2],
+                secondary[2],
+                ternary[2],
+                translation[2],
+            ], [ 
+                0, 0, 0, 1
+            ],
+        ])
+
+    def calc_inverse_normalization_matrix(self) -> ndarray:
+        rotation = self.inverse_matrix[:3, :3]
+        translation = self.inverse_matrix[:3, 3]
+
+        inverse_rotation = rotation.T
+
+        inverse_translation = -inverse_rotation.dot(translation)
+
+        inverse_transformation = identity(4)
+        inverse_transformation[:3, :3] = inverse_rotation
+        inverse_transformation[:3, 3] = inverse_translation
+
+        return inverse_transformation
+
 
 if __name__ == "__main__":
     from main import load_stl
@@ -87,3 +131,4 @@ if __name__ == "__main__":
     bb = BoundingBox(vertebra, center_of_mass=com)
     end = default_timer()
     print(end - start)
+    print(bb.inverse_matrix)
