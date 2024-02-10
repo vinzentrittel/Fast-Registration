5c09233d2bf6bd97cb6e8b7a4fd1c9d76dc8dd58
Vinzent Rittel <vinzentrittel@uni-koblenz.de>
2024-02-10 13:34:49 +0100

    fix bounding box transformation
    Previously the bounding box transformation matrix was calculated with
    the center of mass (CoM) as it's offset. Since the CoM is not
    necessarily the center of the bounding box, this yielded falty results
    in general cases.
    
    This commit fixes the issue and optimizes some duplicate calculations
    away.

--- src/bounding_box.py
+++ src/bounding_box.py
@@ -1,123 +1,142 @@
 from __future__ import annotations
 
-from typing import Tuple
+from typing import Callable, Tuple
 
-from numpy import array, argmax, argmin, cross, identity, ndarray, pi
-from numpy.linalg import norm
+from numpy import (
+    array,
+    argmax,
+    argmin,
+    cross,
+    identity,
+    ndarray,
+    pi,
+    subtract,
+)
+from numpy.linalg import inv, norm
 from scipy.spatial.transform import Rotation
 from vtk import vtkPolyData
 from vtk.util.numpy_support import vtk_to_numpy
 
 class BoundingBox:
-    def __init__(self, geometry: vtkPolyData, center_of_mass: ndarray) -> None:
+    def __init__(self, geometry: vtkPolyData) -> None:
         self.geometry = vtk_to_numpy(geometry.GetPoints().GetData())
-        self.center_of_mass = center_of_mass
+        self.axes = identity(3)
+        self.primary_normal = self.calc_widest_projection(self.geometry)
+        self.secondary_normal = self.calc_outer_points_second_order()
 
-        outer_points_first_order = self.calc_outer_points_first_order()
-        self.primary_normal = outer_points_first_order[1] - outer_points_first_order[0]
-        self.primary_normal /= norm(self.primary_normal)
+        self.ternary_normal = self.calc_normalized_axis(
+            self.secondary_normal,
+            self.primary_normal,
+            operation=cross,
+        )
+        self.secondary_normal = self.calc_normalized_axis(
+            self.ternary_normal,
+            self.primary_normal,
+            operation=cross,
+        )
+        in_place_projection = self.geometry.dot(self.axes)
+        minumums = in_place_projection.min(axis=0)
+        maximums = in_place_projection.max(axis=0)
 
-        outer_points_second_order = self.calc_outer_points_second_order()
-        self.secondary_normal = outer_points_second_order[1] - outer_points_second_order[0]
-        self.secondary_normal /= norm(self.secondary_normal)
-
-        self.ternary_normal = cross(self.secondary_normal, self.primary_normal)
-        self.ternary_normal /= norm(self.ternary_normal)
-
-        self.secondary_normal = cross(self.ternary_normal, self.primary_normal)
-        self.secondary_normal /= norm(self.secondary_normal)
-
-        self.length = self.calc_lengths()
+        self.length = maximums - minumums
+        self.center = self.calc_center(minumums, maximums)
         self.inverse_matrix = self.calc_normalization_matrix()
         self.transform_matrix = self.calc_inverse_normalization_matrix()
 
-    def calc_outer_points_first_order(self) -> ndarray:
-        min_ = [ argmin(self.geometry[:, dimension]) for dimension in range(3) ]
-        max_ = [ argmax(self.geometry[:, dimension]) for dimension in range(3) ]
+        from vtk import vtkSTLWriter, vtkTransform, vtkTransformPolyDataFilter
+        transform = vtkTransform()
+        transform.SetMatrix(self.inverse_matrix.flatten().tolist())
+        transform_filter = vtkTransformPolyDataFilter()
+        transform_filter.SetInputData(geometry)
+        transform_filter.SetTransform(transform)
+        transform_filter.Update()
 
-        min_vertices = array(tuple(self.geometry[vertex] for vertex in min_))
-        max_vertices = array(tuple(self.geometry[vertex] for vertex in max_))
-        widths = norm(max_vertices - min_vertices, axis=0)
-        widest_dimension = argmax(widths)
-        return array([
-            self.geometry[min_[widest_dimension]],
-            self.geometry[max_[widest_dimension]],
-        ])
+    @property
+    def primary_normal(self) -> ndarray:
+        return self.axes[0, :]
+    @primary_normal.setter
+    def primary_normal(self, new_vector: ndarray) -> None:
+        self.axes[0] = new_vector
+    @property
+    def secondary_normal(self) -> ndarray:
+        return self.axes[1]
+    @secondary_normal.setter
+    def secondary_normal(self, new_vector: ndarray) -> None:
+        self.axes[1] = new_vector
+    @property
+    def ternary_normal(self) -> ndarray:
+        return self.axes[2]
+    @ternary_normal.setter
+    def ternary_normal(self, new_vector: ndarray) -> None:
+        self.axes[2] = new_vector
+
+    @staticmethod
+    def calc_normalized_axis(
+        first_vector: ndarray,
+        second_vector: ndarray,
+        operation: Callable[[ndarray, ndarray], ndarray]=subtract,
+    ) -> ndarray:
+        non_normalized_result = operation(first_vector, second_vector)
+        return non_normalized_result / norm(non_normalized_result)
+
+    @classmethod
+    def calc_widest_projection(cls, geometry: ndarray, transform: ndarray=None) -> ndarray:
+        if transform is None:
+            target = geometry
+        else:
+            target = geometry.dot(transform)
+        min_ = argmin(target, axis=0)
+        max_ = argmax(target, axis=0)
+        widest_dimension = argmax(
+            norm(geometry[max_] - geometry[min_], axis=1)
+        )
+        return cls.calc_normalized_axis(
+            geometry[max_[widest_dimension]],
+            geometry[min_[widest_dimension]],
+        )
 
     def calc_outer_points_second_order(self) -> ndarray:
-        orthogonal_axis = cross(self.primary_normal, array([0, 0, 1]))
-        orthogonal_axis /= norm(orthogonal_axis)
+        orthogonal_axis = self.calc_normalized_axis(self.primary_normal, array([0, 0, 1]), cross)
+        candidate_axes = array(tuple(
+            Rotation.from_rotvec(
+                45.0 * n * self.primary_normal,
+                degrees=True,
+            ).apply(
+                orthogonal_axis,
+            )
+            for n in range(4)
+        )).transpose()
 
-        rotations = [Rotation.from_rotvec(pi/4.0 * n * self.primary_normal) for n in range(4)]
-        axis_candidates = [
-            rotation.apply(orthogonal_axis) for rotation in rotations
-        ]
+        return self.calc_widest_projection(self.geometry, transform=candidate_axes)
 
-        projections = array(tuple(self.geometry.dot(axis) for axis in axis_candidates))
-        min_ = argmin(projections, axis=1)
-        max_ = argmax(projections, axis=1)
-        min_vertices = array(tuple(self.geometry[vertex] for vertex in min_))
-        max_vertices = array(tuple(self.geometry[vertex] for vertex in max_))
-        widths = norm(max_vertices - min_vertices, axis=1)
-        widest_axis = argmax(widths)
-        return array([
-            self.geometry[min_[widest_axis]],
-            self.geometry[max_[widest_axis]],
-        ])
-
-    def calc_lengths(self) -> ndarray:
-        projections = array([self.geometry.dot(axis) for axis in (
-            self.primary_normal,
-            self.secondary_normal,
-            self.ternary_normal,
-        )])
-        min_ = projections.min(axis=1)
-        max_ = projections.max(axis=1)
-        return max_ - min_ 
+    def calc_center(self, minimums: ndarray, maximums: ndarray) -> ndarray:
+        return (0.5 * (minimums + maximums)).dot(inv(self.axes))
 
     def calc_normalization_matrix(self) -> ndarray:
-        translation = self.center_of_mass
-        scale = 2.0 / self.length
-
-        # axes
-        primary = scale[0] * self.primary_normal
-        secondary = scale[1] * self.secondary_normal
-        ternary = scale[2] * self.ternary_normal
-        
-        return array([
-            [
-                primary[0],
-                secondary[0],
-                ternary[0],
-                translation[0],
-            ], [
-                primary[1],
-                secondary[1],
-                ternary[1],
-                translation[1],
-            ], [
-                primary[2],
-                secondary[2],
-                ternary[2],
-                translation[2],
-            ], [ 
-                0, 0, 0, 1
-            ],
+        scale = array([
+            2.0 / self.length[0],
+            2.0 / self.length[1],
+            2.0 / self.length[2],
+            1.0,
+        ]) * identity(4)
+        translation = array([
+            [1, 0, 0, -self.center[0]],
+            [0, 1, 0, -self.center[1]],
+            [0, 0, 1, -self.center[2]],
+            [0, 0, 0, 1],
+        ])
+        normalization = array([
+            [ *self.axes[:, 0], 0 ],
+            [ *self.axes[:, 1], 0 ],
+            [ *self.axes[:, 2], 0 ],
+            [ 0, 0, 0, 1 ],
         ])
 
+        matrix = scale.dot(normalization).dot(translation)
+        return matrix
+
     def calc_inverse_normalization_matrix(self) -> ndarray:
-        rotation = self.inverse_matrix[:3, :3]
-        translation = self.inverse_matrix[:3, 3]
-
-        inverse_rotation = rotation.T
-
-        inverse_translation = -inverse_rotation.dot(translation)
-
-        inverse_transformation = identity(4)
-        inverse_transformation[:3, :3] = inverse_rotation
-        inverse_transformation[:3, 3] = inverse_translation
-
-        return inverse_transformation
+        return inv(self.inverse_matrix)
 
 
 if __name__ == "__main__":
@@ -127,8 +146,6 @@ if __name__ == "__main__":
     vertebra = load_stl( '../data/L5.stl')
     from timeit import default_timer
     start = default_timer()
-    com = Slicer.calculate_center_of_mass(vertebra)
-    bb = BoundingBox(vertebra, center_of_mass=com)
+    bb = BoundingBox(vertebra)
     end = default_timer()
     print(end - start)
-    print(bb.inverse_matrix)
