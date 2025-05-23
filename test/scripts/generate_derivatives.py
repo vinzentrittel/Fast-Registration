d9dde1368eb157172f63e81dd98f3eadb99f4ca7
Vinzent Rittel <vinzentrittel@uni-koblenz.de>
2025-05-23 23:19:44 +0200

    fixup! change noise level

--- test/scripts/generate_derivatives.py
+++ test/scripts/generate_derivatives.py
@@ -45,8 +45,9 @@ def apply_multifreq_noise(obj, max_disp, seed):
         amp = 1.0
         freq = base_freq
         n_total = 0.0
-        for _ in range(octaves):
-            n_total += noise.noise(p * freq) * amp
+        for iteration in range(octaves):
+            if iteration != 0:
+                n_total += noise.noise(p * freq) * amp
             freq *= lacunarity
             amp  *= gain
         disp = n_total * max_disp
